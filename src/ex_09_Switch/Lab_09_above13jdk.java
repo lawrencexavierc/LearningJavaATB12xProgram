e817056cc88e6d46145f5f298733612aa2eb77bd
lawrencexavierc <lawmoon1982@gmail.com>
2025-05-24 18:53:33 +0530

    Push to GIT-24-05-25

--- src/ex_09_Switch/Lab_09_above13jdk.java
+++ src/ex_09_Switch/Lab_09_above13jdk.java
@@ -8,6 +8,7 @@ public class Lab_09_above13jdk {
             case 001 -> System.out.println("001");
             case 002 -> System.out.println("002");
             case 003 -> System.out.println("003");
+            case 004 -> System.out.println("004");
         }
     }
 }
